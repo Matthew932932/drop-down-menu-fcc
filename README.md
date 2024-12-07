8eb0236044c844047ae70693358c68477ff324a6
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-12-07 18:03:36 +1100

    html lay out done. Visibility in css toggling by class in js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Webpack-template-Odin
+# drop-down-menu-fcc
 
-Webpack template made from following Odin exercises.
-(fromerly, from project before decided to make it a webpack template: Todo List. Odin Project. Javascript section.)
+Drop Down Menu example from Free Code Camp
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import "./styles.css";
 import odinImage from "./odin-lined.png";
-   
-const image = document.createElement("img");
-image.src = odinImage;
-   
-document.body.appendChild(image);
 
+// const image = document.createElement("img");
+// image.src = odinImage;
+
+// document.body.appendChild(image);
 
 console.log("heyo");
 
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
+
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
