3d066be3cefcb49afe19ce9efb45584843225779
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-12-07 17:19:51 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Webpack-template-Odin
+
+Webpack template made from following Odin exercises.
+(fromerly, from project before decided to make it a webpack template: Todo List. Odin Project. Javascript section.)
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import odinImage from "./odin-lined.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+
+
+console.log("heyo");
+
